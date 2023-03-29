855feb76cbdf50c76423c380d0ee72d30fbef7a2
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-03-29 09:04:22 +0200

    Make CRUD for all Entities

---
+++ src/Form/ReviewType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('message')
+            ->add('note')
+            ->add('userid')
+            ->add('officeid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
